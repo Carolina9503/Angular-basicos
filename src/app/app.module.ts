dd25d02e9c365da8d70d7f34180815062f8bde31
Carolina Daza Gomez <carolina.daza@pragma.com.co>
2022-08-17 00:09:25 -0500

    Inicio de formularios con ngSubmit y [(ngModel)]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterModule } from './counters/counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 
@@ -14,6 +15,7 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
     HeroesModule,
     CounterModule,
+    DbzModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
